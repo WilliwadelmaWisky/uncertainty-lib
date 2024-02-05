3c3579a56e53eaf69d18315b006be9d7a819e261
JS <minecraftprofile52@gmail.com>
2024-02-05 21:47:09 +0200

    README changes + tests

--- README.md
+++ README.md
@@ -5,8 +5,47 @@ Currently supports minmax method and standard uncertainty propagation method.
 
 Work in Progress
 
-Dependencies:
+## Dependencies
 - Numpy
 
-Installation
-- ...
+## Installation
+- Make sure to have dependencies installed to your project
+- Bring the contents of src/ to your project
+	1. Download the zip, unzip and copy the contents of src/ to your project or
+	2. Clone the repo and copy the contents of src/ to your project or
+	3. Copy/Paste the scripts
+
+## Examples
+NOTE: The import statements may vary dependeding on where the files a located!
+
+### Standard-method
+Calculate an uncertainty of a function `f(x, y) = x^2 * y^2` at `{ x = (1.0 ± 0.2), y = (2 ± 0.3) }` using a standard uncertainty propagation method.
+
+```python
+from uncertainty import standard
+from numpy import array
+
+f = lambda x, y: x**2 * y**2
+point = array([1.0, 0.2])
+point_err = array([0.2, 0.3])
+
+err = standard(f, point, point_err)
+```
+
+The `err` variable now has the calculated uncertainty `~3.617`.
+
+### Minmax-method
+Calculate an uncertainty of a function `f(x, y) = x^2 * y^2` at `{ x = (1.0 ± 0.2), y = (2 ± 0.3) }` using a minmax uncertainty propagation method. 
+
+```python
+from uncertainty import minmax
+from numpy import array
+
+f = lambda x, y: x**2 * y**2
+point = array([1.0, 0.2])
+point_err = array([0.2, 0.3])
+
+err = minmax(f, point, point_err)
+```
+
+The `err` variable now has the calculated uncertainty `2`
--- tests/test_uncertainty.py
+++ tests/test_uncertainty.py
@@ -1,5 +1,5 @@
-import numpy as np
 from src.uncertainty import minmax, standard
+from numpy import array
 from math import isclose
 
 
@@ -8,21 +8,10 @@ def test_minmax() -> None:
     A couple of tests for minmax-method
     :return: None
     """
-
-    val = np.array([1, 2])
-    err = np.array([0.2, 0.5])
-
-    result = minmax(lambda x, y: x + y, val, err)
-    assert isclose(result, 0.7)
-
-    result = minmax(lambda x, y: x - y, val, err)
-    assert isclose(result, 0.7)
-
-    val = np.array([1, 2, 3, 4])
-    err = np.array([0.2, 0.5, 0.3, 0.1])
-
-    result = minmax(lambda x, y, z, w: x + y + z + w, val, err)
-    assert isclose(result, 1.1)
+    assert isclose(minmax(lambda x, y: x + y, array([1, 2]), array([0.2, 0.5])), 0.7, abs_tol=0.01)
+    assert isclose(minmax(lambda x, y: x - y, array([1, 2]), array([0.2, 0.5])), 0.7, abs_tol=0.01)
+    assert isclose(minmax(lambda x, y, z, w: x + y + z + w, array([1, 2, 3, 4]), array([0.2, 0.5, 0.3, 0.1])), 1.1, abs_tol=0.01)
+    assert isclose(minmax(lambda x, y: x**2 * y**2, array([1, 2]), array([0.2, 0.3])), 3.617, abs_tol=0.01)
 
 
 def test_standard() -> None:
@@ -30,18 +19,8 @@ def test_standard() -> None:
     A couple of tests for standard-method
     :return: None
     """
-
-    val = np.array([1, 2])
-    err = np.array([0.2, 0.5])
-
-    result = standard(lambda x, y: x + y, val, err)
-    assert isclose(result, 0.5385, abs_tol=0.01)
-
-    result = standard(lambda x, y: x**2 + 2 * y, val, err)
-    assert isclose(result, 1.0770, abs_tol=0.01)
-
-    result = standard(lambda x, y: 3 / 2 * x**4 + 2 / 5 * y**3, val, err)
-    assert isclose(result, 2.6832, abs_tol=0.01)
-
-    result = standard(lambda x, y: x**2 * y, val, err)
-    assert isclose(result, 0.9433, abs_tol=0.01)
+    assert isclose(standard(lambda x, y: x + y, array([1, 2]), array([0.2, 0.5])), 0.5385, abs_tol=0.01)
+    assert isclose(standard(lambda x, y: x**2 + 2 * y, array([1, 2]), array([0.2, 0.5])), 1.0770, abs_tol=0.01)
+    assert isclose(standard(lambda x, y: 3 / 2 * x**4 + 2 / 5 * y**3, array([1, 2]), array([0.2, 0.5])), 2.6832, abs_tol=0.01)
+    assert isclose(standard(lambda x, y: x**2 * y, array([1, 2]), array([0.2, 0.5])), 0.9433, abs_tol=0.01)
+    assert isclose(standard(lambda x, y: x ** 2 * y**2, array([1, 2]), array([0.2, 0.3])), 2, abs_tol=0.01)
